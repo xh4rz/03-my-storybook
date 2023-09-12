450ccab06e5f37848ac7ec9afa590722d4093dba
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-09-12 04:00:26 -0500

    159. Componente - MyLabel

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,5 @@
+import './MyLabel.css';
+
+export const MyLabel = () => {
+	return <span>Hola Mundo</span>;
+};
---
+++ src/stories/MyLabel.stories.ts
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { MyLabel } from '../components/MyLabel';
+
+const meta = {
+	title: 'UI/labels/MyLabel',
+	component: MyLabel,
+	parameters: {
+		layout: 'centered'
+	}
+} satisfies Meta<typeof MyLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {};
