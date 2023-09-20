6027250783d8b12e50789ce07822aab181d3433d
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-09-20 02:58:09 -0500

    165. Github y GitHub Pages

---
+++ docs/assets/preview-953fab1f.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-e3d28cf4.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-0b5b0801.js"),["./DocsRenderer-3PUGWF3O-0b5b0801.js","./iframe-e3d28cf4.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-38063e1e.js","./index-ffc7e5ff.js","./index-167b4c04.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-953fab1f.js.map
